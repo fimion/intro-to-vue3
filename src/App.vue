4c898283d0cb54b991bcbc1818e546c3cdfb0cd9
Alex Riviere <fimion@users.noreply.github.com>
2025-04-05 11:04:20 -0400

    Finished Demo

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,39 @@
+<script>
+let id = 0;
+</script>
 <script setup>
+import { ref, reactive, computed } from "vue";
+import TodoItem from "./components/TodoItem.vue";
 
+const makeTodo = (description) => ({ archived: false, description, id: id++ });
+
+const TodoItems = reactive([]);
+const showArchived = ref(false);
+
+function handleSubmit(event) {
+  const formData = new FormData(event.target);
+  const description = formData.get("description");
+  TodoItems.push(makeTodo(description));
+  event.target.reset();
+}
+
+const filteredItems = computed(() =>
+  TodoItems.filter((item) => item.archived === showArchived.value),
+);
 </script>
 
 <template>
   <h1>My Todo List</h1>
+  <form @submit.prevent="handleSubmit">
+    <label>Add Todo: <input type="text" name="description" /></label>
+    <button>Add!</button>
+  </form>
+  <label><input type="checkbox" v-model="showArchived" /> Show Archived</label>
+  <ul>
+    <li v-for="todoItem in filteredItems" :key="todoItem.id">
+      <todo-item :todo-item="todoItem" @archive="todoItem.archived = true" />
+    </li>
+  </ul>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,11 +1,23 @@
 <script setup>
+import { defineEmits, defineProps } from "vue";
 
+const emit = defineEmits(["archive"]);
+
+defineProps({
+  todoItem: {
+    type: Object,
+    required: true,
+  },
+});
 </script>
 
 <template>
-  <p />
+  <p>
+    <button v-if="!todoItem.archived" type="button" @click="emit('archive')">
+      done!
+    </button>
+    {{ todoItem.description }}
+  </p>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
